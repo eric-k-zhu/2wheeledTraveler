5b82382b7adef99873d3e518ee38fdbf30427d46
Anna Truelove <anna97@live.unc.edu>
2019-10-15 17:58:05 -0400

    added google maps to map page

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,48 @@
-import React from 'react'
-import { Text, Button, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
-export default class MapScreen extends React.Component{
+
+export default class MapScreen extends React.Component {
     static navigationOptions = {
         title: 'Map Screen',
     };
-    render(){
-        return(
-        <View>
-        <Text> Hello this is the Map Screen </Text>
-        <Button onPress={() => this.props.navigation.navigate('ProfileScreen')} title="Back to Profile Page"/>
-        </View>
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <View style={{flex: 0.5, justifyContent: 'center'}}>
+                    <Text style={{textAlign: 'center'}} > Speed        Distance        Time</Text>
+                </View>
+                <View style={{flex: 3}}> 
+                    <MapComponent />
+                </View>
+                <View style={{flex: 0.5, justifyContent: 'center'}}>
+                    <Text style={{textAlign: 'center'}}>Find a Gas Station         Send Location</Text>
+                </View>
+            </View>
         )
+    }
+
+
+}
+
+class MapComponent extends React.Component {
+    render() {
+
+        return (
+
+            <MapView
+                style={{ flex: 1 }}
+                provider={PROVIDER_GOOGLE}
+                showsUserLocation
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421
+                }}
+            />
+        );
     }
 }
