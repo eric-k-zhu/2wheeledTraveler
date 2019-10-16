66cab36d130ae584d8255b253855ab001ad9f6fc
Eric Zhu <ericzhu@live.unc.edu>
2019-10-15 21:51:07 -0400

    add login screen and reroute logout

---
+++ screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, Button, View } from 'react-native';
+
+export default class LoginScreen extends React.Component{
+    static navigationOptions = {
+        title: 'Login Screen',
+    };
+    render(){
+        return(
+        <View>
+        <Text> Hello this is the Login Screen </Text>
+        <Button onPress={() => this.props.navigation.navigate('RegistrationScreen')} title="Register"/>
+        <Button onPress={() => this.props.navigation.navigate('ProfileScreen')} title="Login"/>
+        </View>
+        )
+    }
+}
