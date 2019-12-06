d28a4f24719dbfb39ce3baa8d6d6e75a6df68e10
Anna Truelove <anna97@live.unc.edu>
2019-12-05 21:57:14 -0500

    styling

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import { Button } from "native-base";
 import openMap from 'react-native-open-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { API_KEY } from '../config';
 
 export default class MapScreen extends Component {
-    static navigationOptions = {
-        title: 'Map',
-    };
-
     constructor(props) {
         super(props);
         this.state = {
@@ -27,39 +23,40 @@ export default class MapScreen extends Component {
         this.props.navigation.navigate('RideScreen')
     }
 
+
     render() {
         return (
             <View style={styles.main}>
-                <View style={styles.main}>
-                    <Text style={styles.headingText}>Select Your Destination</Text>
-                    <GooglePlacesAutocomplete
-                        placeholder='Search'
-                        minLength={2} // minimum length of text to search
-                        autoFocus={true}
-                        returnKeyType={'search'} // Can be left out for default return key 
-                        listViewDisplayed={false}    // true/false/undefined
-                        fetchDetails={true}
-                        onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-                            this.createAddress(details.geometry.location);
-                        }
-                        }
+                    <View style={styles.search}>
+                        <Text style={styles.headingText}>Select Your Destination</Text>
+                        <GooglePlacesAutocomplete
+                            placeholder='Search'
+                            minLength={2} // minimum length of text to search
+                            autoFocus={true}
+                            returnKeyType={'search'} // Can be left out for default return key 
+                            listViewDisplayed={false}    // true/false/undefined
+                            fetchDetails={true}
+                            onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                                this.createAddress(details.geometry.location);
+                            }
+                            }
 
-                        query={{
-                            key:  API_KEY,
-                            language: 'en'
-                        }}
+                            query={{
+                                key: API_KEY,
+                                language: 'en'
+                            }}
 
-                        nearbyPlacesAPI='GooglePlacesSearch'
-                        debounce={300}
-                    />
-                </View>
-                <View style={styles.main}>
-                    <Button full rounded success style={styles.button}
-                        onPress={() => this.navigate()}>
-                        <Text style={styles.buttonText}>Start Navigation</Text>
-                    </Button>
-                    <Text>Switch back to our app once navigation has started!</Text>
-                </View>
+                            nearbyPlacesAPI='GooglePlacesSearch'
+                            debounce={300}
+                        />
+                    </View>
+                    <View style={styles.main}>
+                        <Button full rounded success style={styles.button}
+                            onPress={() => this.navigate()}>
+                            <Text style={styles.buttonText}>Start Navigation</Text>
+                        </Button>
+                        <Text style={styles.text}>Switch back to our app once navigation has started!</Text>
+                    </View>
             </View>
         )
     }
@@ -71,7 +68,8 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        marginTop: 30
     },
     title: {
         marginBottom: 20,
@@ -101,6 +99,15 @@ const styles = StyleSheet.create({
     headingText: {
         color: 'black',
         fontWeight: 'bold',
-        fontSize: 24
+        fontSize: 24, 
+        textAlign: 'center', 
+        marginBottom: 30
+    },
+    search: {
+        paddingTop: 30,
+        paddingBottom: 300
+    }, 
+    text: {
+        textAlign: 'center'
     }
 });
