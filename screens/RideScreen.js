1afb4befa096b910c9f603befaa8c3144b1228a3
Anna Truelove <anna97@live.unc.edu>
2019-12-06 20:54:31 -0500

    email location set up

--- screens/RideScreen.js
+++ screens/RideScreen.js
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Button } from "native-base";
 import openMap from 'react-native-open-maps';
 import { API_KEY } from '../config';
+import qs from 'qs';
+import { Linking } from 'react-native';
+import { app } from '../config';
 
 export default class MapScreen extends Component {
 
@@ -19,7 +22,6 @@ export default class MapScreen extends Component {
         // Get current location and pass it to findGas
         navigator.geolocation.getCurrentPosition(
             position => {
-                //this.setState({ current: position });
                 this.setState({ current: position }, function () {
                     this.findGas();
                 });
@@ -69,13 +71,44 @@ export default class MapScreen extends Component {
 
     }
 
-    navGas(){
+    navGas() {
         // navigate to closest gas station
         var address = this.state.lat + "," + this.state.lng;
         openMap({ provider: "google", end: address, navigate_mode: "navigate" });
     }
 
     // work on function to send current location to SMS
+    sendLocation() {
+        // Get current location and pass it to findGas
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ current: position }, function () {
+                    this.sendEmail();
+                });
+            }
+        );
+    }
+
+    sendEmail() {
+
+        let url = `mailto:${"annatruelove97@gmail.com"}`;
+
+        let subject = app.auth().currentUser.displayName + " has sent you their location"
+        let body = "https://www.google.com/maps/place/" + this.state.lat + "," + this.state.lng;
+        
+        // Create email link query
+        const query = qs.stringify({
+            subject: subject,
+            body: body
+        });
+
+        if (query.length) {
+            url += `?${query}`;
+        }
+
+        return Linking.openURL(url);
+    }
+
 
     render() {
         return (
@@ -86,7 +119,9 @@ export default class MapScreen extends Component {
                         <Text style={styles.buttonText}>Find Gas Station</Text>
                     </Button>
 
-                    <Text style={{ textAlign: 'center' }}>Send Location</Text>
+                    <Button full success style={styles.button2} onPress={() => this.sendLocation()}>
+                        <Text style={styles.buttonText}>Send Location</Text>
+                    </Button>
 
                     <Button full rounded success style={styles.button} onPress={() => this.props.navigation.navigate('ProfileScreen')}>
                         <Text style={styles.buttonText}>End Ride</Text>
