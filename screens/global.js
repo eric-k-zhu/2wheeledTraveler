46173b63421ca5204cf7036a38a366d0fc9b34b4
Anna Truelove <anna97@live.unc.edu>
2019-12-08 17:59:04 -0500

    fixed styling on RideScreen

--- screens/global.js
+++ screens/global.js
@@ -3,11 +3,11 @@ import moment from 'moment';
 var time = moment().utcOffset('-05:00').format('hh:mm a');
 
 module.exports = {
-    check1: false,
-    check2: false,
+    check1: true,
+    check2: true,
     check3: false,
     check4: false,
-    check5: false,
+    check5: true,
     curTime: time, 
     curSpeed: 0, 
     maxSpeed: 0
